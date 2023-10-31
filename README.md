156cc80e669cb46a90d6e01ab2c0a623706f3946
Sahil Waikar <sahilwofficial@gmail.com>
2023-10-31 22:12:27 +0530

    Final Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# App Details
+
+Build This wonderful Job serach app using React-Native. I have fetched API provided by RapidAPI, to showcase job opportunities in INDIA. 
+I have also implemented search functionality to make it easier for user to search the job according to the role he/she wants.
+User will also get the job details and if user is eligible and satisfied by the job opportunity , he can apply and then user will be sent
+to the actual job opportunity page , where user can proceed further for next application process.
+
+## Introduction
+
+- develop visually appealing UI/UX designs
+- fetch data from an external API and integrate it
+- implement search & pagination functionality
+- create customAPI data fetching hooks
+- write clean, organized, and maintainable code with proper architecture
+- follow the best practices for React Native development
+
+
---
+++ hook/useFetch.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    
+    params: {
+      query: 'Web developer in India, INDIA',
+      page: '1',
+      num_pages: '1'
+    },
+    headers: {
+      "X-RapidAPI-Key": 'cc06e8cd9bmsh4407e6ba4b39404p10c11ajsne31418de8d2b',
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error)
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
